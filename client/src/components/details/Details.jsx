212e26c3d9cdd03945c1477a17cd386c4032418b
a-stoeva <stoevaaleksandriya@gmail.com>
2025-12-07 14:17:04 +0200

    Add delete and edit buttons to details page with CSS

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, Link, useNavigate } from "react-router";
 
 import styles from "./Details.module.css"
 
@@ -7,19 +7,31 @@ export default function Details() {
 
     const {postId} = useParams();
     const [post, setPost] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`http://localhost:3030/jsonstore/weddingHelper/weddingHelper/${postId}`)
             .then(res => res.json())
             .then(data => setPost(data))
-            .catch(err => console.error(err));
-        console.log(post);
-        
+            .catch(err => console.error(err));       
     }, [postId])
+
+    const deleteClickHandler = async () => {
+
+        try {
+            await fetch(`http://localhost:3030/jsonstore/weddingHelper/weddingHelper/${postId}`, {
+                method: "DELETE",
+            });
+        } catch (err) {
+            alert(err.message);
+        }
+
+        navigate("/catalog");
+    }
 
     return (
             <div className={styles.detailsWrapper}>
-                <img className={styles.image} src={post.image} alt="Peony Flower Candle" />
+                <img className={styles.image} src={post.image} alt={post.title} />
 
                 <div className={styles.info}>
                     <h2 className={styles.title}>{post.title}</h2>
@@ -29,7 +41,14 @@ export default function Details() {
                     <p className={styles.text}>Contact: <span>{post.contact}</span></p>
                     <p className={styles.text}>Location: <span>{post.location}</span></p>
                     <p className={styles.text}>Availability: <span>{post.availability}</span></p>
+                
+
+                <div className={styles.buttons}>
+                    <Link to={`/catalog/${postId}/edit`} className={styles.button}>Edit Post</Link>
+                    <button className={styles.button} onClick={deleteClickHandler}>Delete Post</button>
                 </div>
+                </div>
+
             </div>
     )
 }
